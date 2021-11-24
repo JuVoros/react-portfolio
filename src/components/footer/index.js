2ed86678cf7043dbe960f223e1c2b0c160a5f1bd
JuVoros <Julianvoros@hotmail.com>
2021-11-23 23:17:48 -0800

    added last styling and hover effects and alt image descriptions

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Footer = () => (
 
         <div className="footer">
-            <h1>Made by Julian Voros</h1>
+            <h1>Made by: Julian Voros 🐼</h1>
         </div>
 );
 export default Footer;
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -8,7 +8,7 @@ const Contact= () => {
     return (
         <div className="ContactContainer">
             <div>
-                <h2>
+                <h2 className="Contacts">
                     CONTACTS
                 </h2>
             </div>
@@ -16,17 +16,17 @@ const Contact= () => {
         <div className="ContactContainer2">
             <section className="PCard">
                 <h3 className="ContactTitle">GitHub</h3>
-                <img src={Github}/>
+                <img src={Github} alt="GitHub Logo"/>
                 <a href="https://github.com/JuVoros" className="ContactBtn">JuVoros</a>
             </section>
             <section className="ResumeCard">
                 <h3 className="ContactTitle">Email</h3>
-                <img src={Email}/>
+                <img src={Email} alt="Email Logo"/>
                 <a href="mailto:JulianVoros@Hotmail.co," className="ContactBtn">JulianVoros@Hotmail.com</a>
             </section>
             <section className="PCard">
                 <h3 className="ContactTitle">linkedIn</h3>
-                <img src={Linked}/>
+                <img src={Linked} alt="LinkedIn Logo"/>
                 <a href="mailto:JulianVoros@Hotmail.co," className="ContactBtn">Julian Voros</a>
             </section>
             </div>
